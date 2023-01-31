df1e11e1de7ef29333e94e20f2dd4d3ffb26ccd2
sundayirvine-code <amugumbiirvine@gmail.com>
2023-01-31 04:33:33 -0800

    created 100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,12 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/([a-zA-Z0-9:-\]\[]+)from:([a-zA-Z0-9]+)\]/).map { |match| match[2] }.join
+
+log_line = ARGV[0]
+
+# Extract sender, receiver, and flags from log line using regular expressions
+sender = log_line[/\[from:([a-zA-Z0-9\+]+)\]/, 1]
+receiver = log_line[/\[to:([a-zA-Z0-9\+]+)\]/, 1]
+flags = log_line[/\[flags:([-0-9:]+)\]/, 1]
+
+# Output the results in the specified format
+puts "#{sender},#{receiver},#{flags}"
+

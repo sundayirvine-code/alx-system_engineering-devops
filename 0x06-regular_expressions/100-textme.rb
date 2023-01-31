22d1fa8ff57c63b9a2cd16316d8779f1ccd29425
Irvine Sunday <amugumbiirvine@gmail.com>
2023-01-31 15:16:38 +0300

    Update 100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/([a-zA-Z0-9:-\]\[]+)from:([a-zA-Z0-9]+)]([a-zA-Z0-9:-\]\[]+)/.group(2)).join
+puts ARGV[0].scan(/([a-zA-Z0-9:-\]\[]+)from:([a-zA-Z0-9]+)\]/).map { |match| match[2] }.join
